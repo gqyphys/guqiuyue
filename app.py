12d8ef09f39bd5b6b81b045b3745b25eabbb5501
guqiuyue <gqyphy911@163.com>
2016-11-19 21:38:13 +0800

    a

---
+++ app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import os, time, json
+
+from flask import Flask, render_template
+
+# -- create app --
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
