afa45a4533ea3f1c87de51d7cdacbe0047116507
guqiuyue <gqyphy911@163.com>
2016-12-28 13:38:06 +0800

    a

--- static/js/save_file.php
+++ static/js/save_file.php
@@ -1,7 +1,7 @@
 <?php
 # Save the audio to a URL-accessible directory for playback.
 parse_str($_SERVER['QUERY_STRING'], $params);
-$name = isset($params['name']) ? $params['name'] : 'output.wav';
+$name = isset($params['name']) ? $params['name'] : 'temp.wav';
 $content = file_get_contents('php://input');
 $fh = fopen($name, 'w') or die("can't open file");
 fwrite($fh, $content);
