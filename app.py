7e7d3988b3dca0d29df2980ec3136c62a3e688e3
guqiuyue <gqyphy911@163.com>
2017-04-17 10:18:47 +0800

    a

--- app.py
+++ app.py
@@ -8,10 +8,13 @@ from flask import Flask, render_template
 # -- create app --
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/recorder/')
+def recorder():
+    return render_template('recorder.html')
 
 
 if __name__ == '__main__':
